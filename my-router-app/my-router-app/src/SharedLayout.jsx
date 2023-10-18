5d15409cf3b721dee537c0ac3c13dc7235a47a31
Tomilola Adelaja <adelajatomilola@gmail.com>
2023-10-18 21:45:40 +0100

    Additional files

--- my-router-app/my-router-app/src/SharedLayout.jsx
+++ my-router-app/my-router-app/src/SharedLayout.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Navigation from './components/nav-menu'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Navigation from "./components/nav-menu";
+import { Outlet } from "react-router-dom";
+import { Protected } from "./components/ProtectedRoute";
+import { useAuth } from "./components/AuthContext";
 
 const SharedLayout = () => {
+  const {user} = useAuth()
   return (
     <>
-        <Navigation/>
-        <Outlet/>
+      {/* <Navigation /> */}
+      <Protected isSignedIn={user ? true : false}>
+        <Outlet />
+      </Protected>
     </>
   );
 };
 
-export default SharedLayout
+export default SharedLayout;
--- my-router-app/my-router-app/src/components/nav-menu.js
+++ my-router-app/my-router-app/src/components/nav-menu.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from './AuthContext';
 
 const Navigation = () => {
+  const {user} = useAuth()
+  const navigate = useNavigate()
+  const {logout} = useAuth()
+  const handleLogout = (e) => {
+    e.preventDefault();
+    logout()
+    navigate('/login')
+}
   return (
+    
     <nav>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
+
+
+        {user && <li>
           <Link to="profile">Profile</Link>
+        </li>}
+        <li>
+          <Link to="login">Login</Link>
         </li>
+        {user && <li onClick={handleLogout}>
+          Logout
+        </li>}
       </ul>
     </nav>
   );
